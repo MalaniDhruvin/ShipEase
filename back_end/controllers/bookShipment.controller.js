da6468d089f353137189a9b574d5d7e804c78125
niravpansheriya <niravpanseriya2004@gmail.com>
2024-07-14 10:30:54 +0530

    uncleared cookies

--- back_end/controllers/bookShipment.controller.js
+++ back_end/controllers/bookShipment.controller.js
@@ -7,63 +7,66 @@ const { v4: uuidv4, validate } = require('uuid');
 
 
 exports.bookShipment = asyncHandler(async (req, res) => {
-    const user = await User.findById(req.user._id);
-    if (!user){
-        throw new ApiError(500, "Registration is Required to Book a Shipment");
-    }
-    const userId = user._id;
-    if (!userId){
-        throw new ApiError(400, "Registration is Required to Book Shipment");
-    }
-    const {origin, destination, weight, pickup_date, delivery_date, cost} = req.body;
-
-    if (
-        Object.values(origin).some((field) => field === undefined)
-    ){
-        throw new ApiError(400, "Origin Address Fields are Missing")
-    }
-    if (
-        Object.values(destination).some((field) => field === undefined)
-    ){
-        throw new ApiError(400, "Some Address Fields are Missing")
-    }
-    if (
-        [weight, pickup_date, delivery_date, cost].some((field) => field === undefined)
-    ){
-        throw new ApiError(400, "Some Fields are Missing")
-    }
+    try {
+        const user = await User.findById(req.user._id);
+        if (!user){
+            throw new ApiError(500, "Registration is Required to Book a Shipment");
+        }
+        const userId = user._id;
+        if (!userId){
+            throw new ApiError(400, "Registration is Required to Book Shipment");
+        }
+        const {origin, destination, weight, pickup_date, delivery_date, cost} = req.body;
     
-
-    const trackingId = uuidv4();
-
-    const shipment = await Shipment.create({
-        userId,
-        trackingId,
-        origin,
-        destination,
-        weight,
-        status: 'pending',
-        pickup_date,
-        delivery_date,
-        cost,
-    })
+        if (
+            Object.values(origin).some((field) => field === undefined)
+        ){
+            throw new ApiError(400, "Origin Address Fields are Missing")
+        }
+        if (
+            Object.values(destination).some((field) => field === undefined)
+        ){
+            throw new ApiError(400, "Some Address Fields are Missing")
+        }
+        if (
+            [weight, pickup_date, delivery_date, cost].some((field) => field === undefined)
+        ){
+            throw new ApiError(400, "Some Fields are Missing")
+        }
+        
     
-    if(!shipment){
-        throw new ApiError(500, "Shipment is not Registered in database");
+        const trackingId = uuidv4();
+    
+        const shipment = await Shipment.create({
+            userId,
+            trackingId,
+            origin,
+            destination,
+            weight,
+            status: 'pending',
+            pickup_date,
+            delivery_date,
+            cost,
+        })
+        
+        if(!shipment){
+            throw new ApiError(500, "Shipment is not Registered in database");
+        }
+    
+        await user.shipments.push(shipment._id);
+        await user.save({ validateBeforeSave: false });    
+    
+        return res
+        .status(201)
+        .json(
+            new ApiResponse(201, shipment, "Shipment Registerd Successfully")
+        )
+    } catch (error) {
+        if (error instanceof ApiError) {
+            return next(error);
+        } else {
+            console.error("Unexpected error: ", error);
+            return next(new ApiError(500, "An unexpected error occurred"));
+        }        
     }
-
-    await user.shipments.push(shipment._id);
-    await user.save({ validateBeforeSave: false });
-
-    const options = {
-        httpOnly: true,
-        secure: true,
-    }
-    return res
-    .status(201)
-    .clearCookie("accessToken", options)
-    .clearCookie("refreshToken", options)
-    .json(
-        new ApiResponse(201, shipment, "Shipment Registerd Successfully")
-    )
 })
--- back_end/controllers/getShipments.controller.js
+++ back_end/controllers/getShipments.controller.js
@@ -19,17 +19,13 @@ exports.getShipments = asyncHandler(async(req, res) => {
             }      
             shipments.push(shipment);
         }
-        const options = {
-            httpOnly: true,
-            secure: true,
-        }
+
         return res
         .status(200)
-        .clearCookie("accessToken", options)
-        .clearCookie("refreshToken", options)
         .json(
             new ApiResponse(200, shipments, "Shipments Retrieved")
         )
+        
     } catch (error) {
         if (error instanceof ApiError) {
             return next(error);
